d16f6bf33d263128108db87e8cb51ba1d0e0baa2
Jakob Bal <baljakob9@gmail.com>
2024-03-27 14:49:04 +0100

    Moved entries dropdown and Map component to App in order to use WorkBox in a better way

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,30 @@ import 'leaflet/dist/leaflet.css';
 
 import WorkBox from './WorkBox.tsx';
 import Map from './Map.tsx';
+import { dropDownEntry } from './types.ts';
+import MapDropdown from './MapDropdown.tsx';
 
 function App() {
+  const entries: Array<dropDownEntry> = [
+    { name: 'OpenStreetMap Tileset', value: 'osm' },
+    { name: 'Bundesländer GeoJSON', value: 'geojson' },
+  ];
+  const [selected, setSelected] = useState(
+    entries && entries.length > 0 ? entries[0] : { name: '', value: '' }
+  );
+  const MapComponent = () => (
+    <Map visible={true} mapLayer={selected.value}></Map>
+  );
+  const Dropwdown = () => (
+    <MapDropdown
+      entries={entries}
+      selected={selected}
+      setSelected={(val: dropDownEntry) => setSelected(val)}
+    ></MapDropdown>
+  );
   return (
     <>
-      <WorkBox Element={Map}></WorkBox>
+      <WorkBox Element={MapComponent} UiElements={[Dropwdown]}></WorkBox>
     </>
   );
 }
