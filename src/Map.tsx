e8c1876e44daf4b8e2e71deaedd99abee6b7f31b
Jakob Bal <baljakob9@gmail.com>
2024-03-27 14:51:11 +0100

    Cleaned up Map component and added geojson layer

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,35 +1,51 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import CircleMark from './CircleMark';
 import { question } from './types';
-import { LatLngExpression } from 'leaflet';
+import { LatLngExpression, map } from 'leaflet';
 
 import * as data from './data/fr41.json';
-import { useState } from 'react';
+import bundeslaender from './data/bundeslaender.geojson.json';
+
+import { useEffect, useState } from 'react';
 
 type MapProps = {
   visible: boolean;
+  mapLayer: string;
 };
 
-export default function Map() {
+export default function Map({ visible, mapLayer }: MapProps) {
   const [zoom, setZoom] = useState(7);
+  const [bL, setBl] = useState({} as GeoJSON.FeatureCollection); // Bundesland
   const position: LatLngExpression = [47.5939, 14.1245];
   const dataList: question = data as question;
+  console.log(visible);
   return (
     <MapContainer
       maxZoom={10}
-      className={`flex-auto h-5/6 w-full relative ${
+      className={`-z-0 flex-auto h-5/6 w-full relative ${
         true ? 'visible' : 'hidden'
       }`}
       center={position}
       zoom={zoom}
       scrollWheelZoom={true}
     >
-      <TileLayer
-        className=''
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
+      {mapLayer === 'geojson' ? (
+        <GeoJSON
+          attribution='&copy; SFB/DiÖ'
+          data={bundeslaender as GeoJSON.FeatureCollection}
+        />
+      ) : (
+        <TileLayer
+          className=''
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      )}
+
       <CircleMark dataList={dataList} showPropCircles={true} />
     </MapContainer>
   );
 }
+
+/*
+ */
