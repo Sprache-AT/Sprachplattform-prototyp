c235386990dc802acf2a32ce209368a1674028f8
Jakob Bal <baljakob9@gmail.com>
2024-04-12 12:44:54 +0200

    Moved data processing up

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, useMap } from 'react-leaflet';
 import CircleMark from './CircleMark';
 import { evaluatedAnswer, question } from './types';
 import { LatLngExpression, map } from 'leaflet';
@@ -7,30 +7,28 @@ import * as data from './data/fr41.json';
 import bundeslaender from './data/bundeslaender.geojson.json';
 import dialectregions from './data/All_Dialektregionen_no_formatting.geojson.json';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Legend from './Legend';
-import { evaluateQuestion } from './service/EvaluateData';
 
 type MapProps = {
   visible: boolean;
   mapLayer: string;
+  showDialect: boolean;
+  usedColors: Map<string, string> | undefined;
+  evaluatedData: Array<evaluatedAnswer> | undefined;
 };
 
-export default function Map({ visible, mapLayer }: MapProps) {
+export default function Map({
+  visible,
+  mapLayer,
+  showDialect,
+  usedColors,
+  evaluatedData,
+}: MapProps) {
   const [zoom, setZoom] = useState(7);
   const [bL, setBl] = useState({} as GeoJSON.FeatureCollection); // Bundesland
-  const [evaluatedData, setEvaluatedData] = useState<
-    Array<evaluatedAnswer> | undefined
-  >(undefined);
-  const [usedColors, setUsedColors] = useState<Map<string, string>>();
   const position: LatLngExpression = [47.5939, 14.1245];
   const dataList: question = data as question;
-  console.log(visible);
-  useEffect(() => {
-    const { data, colors } = evaluateQuestion(dataList);
-    setEvaluatedData(data);
-    setUsedColors(colors);
-  }, []);
   return (
     <MapContainer
       maxZoom={10}
@@ -42,14 +40,14 @@ export default function Map({ visible, mapLayer }: MapProps) {
       scrollWheelZoom={true}
     >
       <Legend colors={usedColors} />
-      {mapLayer === 'dialect' ? (
-        <GeoJSON
-          attribution='&copy; SFB/DiÖ'
-          data={dialectregions as GeoJSON.FeatureCollection}
-        />
-      ) : (
-        ''
-      )}
+      <GeoJSON
+        style={() => {
+          return { className: `${showDialect ? 'visible' : 'hidden'}` };
+        }}
+        attribution='&copy; SFB/DiÖ'
+        data={dialectregions as GeoJSON.FeatureCollection}
+      />
+
       {mapLayer === 'geojson' ? (
         <GeoJSON
           attribution='&copy; SFB/DiÖ'
