c34fae678695cbdf74fb4b9dd769594dc3f7d5c3
Jakob Bal <baljakob9@gmail.com>
2024-04-12 12:44:23 +0200

    Created checkbox component

---
+++ src/CheckboxComp.tsx
@@ -0,0 +1,27 @@
+interface CheckboxCompProps {
+  label: string;
+  onChange: (val: boolean) => void;
+}
+
+export default function CheckboxComp({ label, onChange }: CheckboxCompProps) {
+  function handleChange(val: boolean) {
+    console.log(val);
+    onChange(val);
+  }
+  return (
+    <>
+      <input
+        id='default-checkbox'
+        type='checkbox'
+        onChange={(e) => handleChange(e.target.checked)}
+        className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+      />
+      <label
+        htmlFor='default-checkbox'
+        className='ms-2 text-sm font-medium text-white dark:text-gray-300'
+      >
+        {label}
+      </label>
+    </>
+  );
+}
