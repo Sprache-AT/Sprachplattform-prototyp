40144b39d4747e1b031718e91a2cc1146219bb3c
Jakob Bal <baljakob9@gmail.com>
2024-05-06 08:50:13 +0200

    Added baseURL to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://sprachplattform.acdh-ch-dev.oeaw.ac.at',
   preview: {
     port: 8080,
     strictPort: true,
