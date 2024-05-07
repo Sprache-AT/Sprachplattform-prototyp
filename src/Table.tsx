cd19106bf9363ecba446c22c6077792921e1b0e5
Jakob Bal <baljakob9@gmail.com>
2024-05-07 08:43:50 +0200

    Added content to TableComponent

--- src/Table.tsx
+++ src/Table.tsx
@@ -4,15 +4,17 @@ interface MapProps {
   headerTitle: string;
   headerDesc: string;
   tableHeads: Array<{ title: string; isSortable: boolean }>;
+  tableContent: Array<Array<string>>;
 }
 
 export default function Table({
   headerTitle,
   headerDesc,
   tableHeads,
+  tableContent,
 }: MapProps) {
   return (
-    <div className='relative overflow-x-auto overflow-y-auto shadow-md sm:rounded-lg'>
+    <div className='relative h-[50vh] overflow-x-auto overflow-y-scroll shadow-md sm:rounded-lg'>
       <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
         <caption className='p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800'>
           {headerTitle}
@@ -20,19 +22,30 @@ export default function Table({
             {headerDesc}
           </p>
         </caption>
-        <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+        <thead className='sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
           <tr>
             {tableHeads.map((head, index) => (
               <th scope='col' key={`header-${index}`} className='px-6 py-3'>
-                {head.title} {head.isSortable && <FaSort color='black' />}
+                <div className='flex items-center space-x3'>
+                  {head.title} {head.isSortable && <FaSort color='black' />}
+                </div>
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-60'>
-            <td className='px-6 py-4'>Test</td>
-          </tr>
+          {tableContent.map((row, index) => (
+            <tr
+              key={`row-${index}`}
+              className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-60'
+            >
+              {row.map((cell, idx) => (
+                <td key={`cell-${index}-${idx}`} className='px-6 py-4'>
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
